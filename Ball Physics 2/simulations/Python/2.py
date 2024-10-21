97a5c89521484ccd67728eeae7243ae90f0fe971
Eli <102241296+Penguins688@users.noreply.github.com>
2024-10-20 19:16:01 -0700

    Delete Ball Physics 2/simulations/Python directory

--- Ball Physics 2/simulations/Python/2.py
+++
@@ -1,3 +0,0 @@
-with open("simulations/2.ball", "w") as file:
-    for i in range(600):
-        file.write(f"395 {i} 10 0 0\n")
--- Ball Physics 2/simulations/Python/6.py
+++
@@ -1,5 +0,0 @@
-import math
-
-with open("simulations/6.ball", "w") as file:
-    for i in range(600):
-        file.write(f"{700 + math.sin(i)*10} {i} 10 -100 0\n")
